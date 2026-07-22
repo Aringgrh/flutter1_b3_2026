5716bc933e650e4533950a11fb4f2337e8ebe698
Aringgrh <Aaahary14@gmail.com>
2026-07-22 14:50:53 +0700

    tugas 2 flutter

---
+++ lib/tugas_1/tugas_2_flutter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Tugas2 extends StatelessWidget {
+  const Tugas2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: 
+      Text("Tentang Aplikasi"),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+      ),
+
+      body: Column( 
+        children: [Align(alignment: AlignmentGeometry.topCenter),
+        
+        Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Text("Food Saver", style: 
+          TextStyle(fontSize: 34),),
+        )
+        
+      ],),
+    );
+  }
+}
