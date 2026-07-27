929270ac8523af1da5211dfeaf7697a461d149c8
Aringgrh <Aaahary14@gmail.com>
2026-07-27 09:43:15 +0700

    tugas 4 flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter1_b3_2026/tugas_1/tugas_3_flutter.dart';
+import 'package:flutter1_b3_2026/tugas_1/tugas_4_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: Tugas3Flutter(),
+      home: Tugas4Flutter(),
     );
   }
 }
